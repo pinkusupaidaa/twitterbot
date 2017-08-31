6875d934d07d13101b0e3a8ff80ad73dadc438a8
pinkusupaidaa <starck88@gmail.com>
2017-08-31 12:31:22 +0200

    Update ZeBot.py

--- Twitter Bot/ZeBot.py
+++ Twitter Bot/ZeBot.py
@@ -11,5 +11,5 @@ auth_obj = bc.authorization(config.consumer_key, config.consumer_secret, config.
 now = datetime.datetime.now()
 todays_date = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
 
-ze_bot = bc.TwitterBot(todays_date , "#skattärstöld", auth_obj)
+ze_bot = bc.TwitterBot(todays_date , "#svpol", auth_obj)
 ze_bot.start_retweeting()
